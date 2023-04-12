3212ab46ecef87825ec234740727c2656f54c901
surya_rahul <dsrahul30@gmail.com>
2023-04-12 15:22:21 +0530

    Assignments upto 5

---
+++ assignment2/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", {id: "parent"}, 
+    [
+        React.createElement("div", {id: "child"}, 
+            [React.createElement("h1", {}, "I am an H1 Tag"), 
+            React.createElement("h2", {}, "I am an H2 Tag")]
+        ),
+        React.createElement("div", {id: "child2"}, 
+            [React.createElement("h1", {}, "I am an H1 Tag"), 
+            React.createElement("h2", {}, "I am an H2 Tag")]
+        )
+    ]
+);
+
+// const heading = React.createElement(
+//     "h1", 
+//     {id: 'heading', xyz: 'abc'}, 
+//     "Hello World! from React"
+// );
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent);
---
+++ assignment2/README.md
@@ -0,0 +1,20 @@
+# Namste React
+
+# Parcel
+- Dev build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - Support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different Dev and Prod bundles
